7e6dd169ef5b62c4847a0c46ddc1f2dd09653635
Jieun Song <wldms2494@gmail.com>
2025-12-02 10:24:27 +0900

    similar products 생성

---
+++ src/lwc/similarProducts/similarProducts.js
@@ -0,0 +1,13 @@
+/**
+ * Project Name : The South Face
+ * File Name : similarProducts
+ * Description : 필수작성
+ * Copyright : Copyright © JieunSong. All Rights Reserved. 2025
+ * Author : 82107
+ * Created Date : 2025-12-02 오전 10:12
+ */
+import {LightningElement} from 'lwc';
+
+export default class SimilarProducts extends LightningElement {
+
+}
