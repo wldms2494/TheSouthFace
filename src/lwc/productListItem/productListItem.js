b0d9d2c08824eeaadb93d8d507eae71a16239026
Jieun Song <wldms2494@gmail.com>
2025-12-02 16:27:46 +0900

    NavigationMixin을 이용하여 RecordPage로 이동

--- src/lwc/productListItem/productListItem.js
+++ src/lwc/productListItem/productListItem.js
@@ -7,10 +7,24 @@
  * Created Date : 2025-12-02 오후 1:32
  */
 import {LightningElement, api} from 'lwc';
+import { NavigationMixin} from "lightning/navigation";
+import PRODUCT_OBJECT from '@salesforce/schema/Product__c';
 
-export default class ProductListItem extends LightningElement {
+export default class ProductListItem extends NavigationMixin(LightningElement) {
     @api product;
-    renderedCallback() {
-        console.log('product:'+JSON.stringify(this.product));
+
+    // LWC에서 선언적 방식으로 SFDC 페이지 이동 기능을 사용할 수 있도록 제공하는 표준 모듈 => NavigationMixin
+    // 레코드페이지, 목록페이지, 웹페이지 등으로 이동할 수 있는 기능을 제공
+    handleViewDetailsClick(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes:{
+                recordId: this.product.Id,
+                objectApiName: PRODUCT_OBJECT.objectApiName,
+                actionName: 'view'
+            }
+
+        });
     }
+
 }
