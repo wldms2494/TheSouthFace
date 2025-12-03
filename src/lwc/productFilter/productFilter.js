9271806b9a3d362dee4c39ad54857de53881d617
Jieun Song <wldms2494@gmail.com>
2025-12-03 10:28:09 +0900

    Product Filter 껍데기 완성

---
+++ src/lwc/productFilter/productFilter.js
@@ -0,0 +1,13 @@
+/**
+ * Project Name : The South Face
+ * File Name : productFilter
+ * Description : 필수작성
+ * Copyright : Copyright © JieunSong. All Rights Reserved. 2025
+ * Author : 82107
+ * Created Date : 2025-12-03 오전 10:10
+ */
+import {LightningElement} from 'lwc';
+
+export default class ProductFilter extends LightningElement {
+
+}
