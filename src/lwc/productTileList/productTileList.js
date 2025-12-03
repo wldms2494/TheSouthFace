d801dc7b56cb5cfde412f40355ce66b4578459de
Jieun Song <wldms2494@gmail.com>
2025-12-03 16:20:00 +0900

    filter를 받는 getProducts 완성

---
+++ src/lwc/productTileList/productTileList.js
@@ -0,0 +1,13 @@
+/**
+ * Project Name : The South Face
+ * File Name : productTileList
+ * Description : 필수작성
+ * Copyright : Copyright © JieunSong. All Rights Reserved. 2025
+ * Author : 82107
+ * Created Date : 2025-12-03 오후 3:51
+ */
+import {LightningElement} from 'lwc';
+
+export default class ProductTileList extends LightningElement {
+
+}
